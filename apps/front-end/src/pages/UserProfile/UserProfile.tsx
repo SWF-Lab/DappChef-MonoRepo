ea8abde1de154454b50798ce9136fcb7a85e7001
ChiHaoLu <b08303113@ntu.edu.tw>
2023-02-16 01:33:44 +0800

    add user profile

--- apps/front-end/src/pages/UserProfile/UserProfile.tsx
+++ apps/front-end/src/pages/UserProfile/UserProfile.tsx
@@ -1,15 +1,128 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { ethers } from "ethers"
 import { TextExample } from "src/components/TextExample/TextExample"
 import { Container } from "./styles"
+import REWARD_NFT_ABI from "../../../contract-artifacts/rewardABI.json"
 
 export const UserProfile = () => {
+  const RewardNFTAddress = "0xaFAD4dC9C0f1D05bcB6a2dfa5123bbd27284C8d3"
+  const PROBLEMS_IPFS_CID =
+    "https://nftstorage.link/ipfs/bafkreias5uyk5qwoh2iwzgxriavjvlntzhkig6c7m7ygipislddf4slvqi"
+
+  const [account, setAccount] = useState("")
+  const [status, setStatus] = useState([])
+  const [nftImageList, setNFTImageList] = useState<Blob[]>([])
+  const [problemsInfo, setProblemsInfo] = useState([])
+  const [problemList, setProblemList] = useState<any>([])
+  const [loading, setLoading] = useState(true)
+
+  const getProblems = async () => {
+    const problemsResponse = await fetch(PROBLEMS_IPFS_CID)
+    const data = await problemsResponse.json()
+    setProblemsInfo(data)
+    // console.log(data)
+    const pList = Object.values(data)
+    setProblemList(pList)
+    // console.log(pList)
+  }
+
+  async function getTokenInfoOfUser() {
+    /** --------------------------------------------------------
+     * Setting up the basic ethers object
+     * -------------------------------------------------------- */
+
+    await window.ethereum.enable()
+    const provider = new ethers.providers.Web3Provider(
+      (window as any).ethereum,
+      "goerli"
+    )
+
+    provider
+      .send("eth_requestAccounts", [])
+      .then((accounts) => {
+        if (accounts.length > 0) setAccount(accounts[0])
+      })
+      .catch((e) => console.log(e))
+
+    const signer = provider.getSigner()
+
+    /**  --------------------------------------------------------
+     * Get RewardNFT Contract Object
+     * -------------------------------------------------------- */
+
+    const RewardNFTContract = new ethers.Contract(
+      RewardNFTAddress,
+      REWARD_NFT_ABI,
+      signer
+    )
+
+    /**  --------------------------------------------------------
+     * Get User Answer Status
+     * -------------------------------------------------------- */
+    const signerAddress = await signer.getAddress()
+    const TargetAccountBalance = await RewardNFTContract.getSolvingStatus(
+      signerAddress
+    )
+    console.log(TargetAccountBalance)
+    const nSolvingProb = TargetAccountBalance[0]
+    const solvingProb = TargetAccountBalance[1]
+    const temp = solvingProb.map(Number)
+    // console.log(temp.slice(0, nSolvingProb.toNumber()))
+    setStatus(temp)
+
+    for (let i = 0; i < nSolvingProb; i++) {
+      console.log(solvingProb[i].toNumber())
+      // Get the Target Problem TokenID of the User
+      const TargetAccount_TargetProblem_TokenID =
+        await RewardNFTContract.getTokenID(
+          signerAddress,
+          solvingProb[i].toNumber()
+        )
+      // console.log(TargetAccount_TargetProblem_TokenID)
+
+      // Get the Target Problem TokenURI of the User
+      const TargetAccount_TargetProblem_TokenURI =
+        await RewardNFTContract.tokenURI(TargetAccount_TargetProblem_TokenID)
+      // console.log(TargetAccount_TargetProblem_TokenURI)
+
+      const problemsResponse = await fetch(TargetAccount_TargetProblem_TokenURI)
+      const data = await problemsResponse.json()
+      const imageCID = data.image.toString()
+
+      const imageResponse = await fetch(imageCID)
+      const image = await imageResponse.blob()
+
+      setNFTImageList((nftImageList) => [...nftImageList, image])
+      console.log(image)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getProblems()
+    getTokenInfoOfUser()
+  }, [])
+
   return (
     <div>
       <Container>
         <TextExample>User Profile</TextExample>
       </Container>
       <Container>
-        <div>TODO: User Profile</div>
+        <p>{account}</p>
+        {loading ? (
+          <>Loading...</>
+        ) : (
+          <>
+            {nftImageList.map((image, i) => (
+              <div>
+                <p>{i}</p>
+                <img src={URL.createObjectURL(image)} width="50" height="58" />
+              </div>
+            ))}
+          </>
+        )}
       </Container>
     </div>
   )
